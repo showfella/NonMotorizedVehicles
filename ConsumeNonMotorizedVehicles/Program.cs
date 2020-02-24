d7222be904c64406a5e9bc18dd59ff2785f4a308
showfella <59898522+showfella@users.noreply.github.com>
2020-02-24 13:48:32 -0800

    Add project files.

---
+++ ConsumeNonMotorizedVehicles/Program.cs
@@ -0,0 +1,40 @@
+﻿using Enumerations;
+using NonMotorizedVehicles;
+using Structures;
+using System;
+
+namespace ConsumeNonMotorizedVehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                    Bicycle bike = new Bicycle
+                    {
+                        TerrainType = "Beach",
+                        NoOfTires = 2
+                    };
+
+                    Tricycle trike = new Tricycle
+                    {
+                        TerrainType = "City",
+                        NoOfTires = 3
+                    }
+
+                    Bicycle bikeTwin = bike;
+                    bikeTwin.Terraintype = TerrainType.AllTerrain;
+                    Console.WriteLine($"bike has terrain type {bike.TerrainType}");
+
+                    Tricycle trikeTwin = trike;
+                    autoTwin.Terraintype = TerrainType.Mountain;
+                    Console.WriteLine($"trike has terrain type {trike.TerrainType}");
+            }
+            catch (Exception e)
+            { throw e; }
+
+
+        }
+    }
+}
---
+++ NonMotorizedVehicles/Structures.cs
@@ -0,0 +1,13 @@
+﻿using Enumerations;
+
+namespace Structures
+{
+    public struct Tricycle : INonMotorizedVehicle
+    {
+        TerrainType TerrainType { get; set; }
+        public int NoOfTires { get; set; }
+        public string Brakes { get; set; }
+        
+
+    }
+}
